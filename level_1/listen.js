d5c67d6f1609131a243d8e9ee90c4aa19a984488
Christian D. Lamothe <c.lamothe25@gmail.com>
2017-08-26 14:47:30 +0000

    Initial commit

---
+++ level_1/listen.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+
+app.get('/locations', function(request, response) {
+  response.send(["Caspiana", "Indigo", "Paradise"]);
+}); 
+app.listen(3001, function(){
+    console.log("Running Express"); 
+ });

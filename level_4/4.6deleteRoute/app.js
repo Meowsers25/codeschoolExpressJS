5287e0c61e150454a4f34700308ed5397a95865d
Christian D. Lamothe <c.lamothe25@gmail.com>
2017-09-07 01:49:23 +0000

    redo of level 4

--- level_4/4.6deleteRoute/app.js
+++ level_4/4.6deleteRoute/app.js
@@ -1,3 +1,6 @@
+/*Create a Dynamic Route for deleting cities and handle for
+cities that are not in our list.*/
+
 var express = require('express');
 var app = express();
 
@@ -12,33 +15,32 @@ var cities = {
 app.param('name', function (request, response, next) {
   request.cityName = parseCityName(request.params.name);
 });
-  
-  
-/*Create a DELETE route that takes the city name as its 
-first argument, followed by a callback that takes a 
-request and response objects as arguments.*/      
+/*Create a DELETE route that takes the city name as its
+first argument, followed by a callback that takes a request
+and response objects as arguments.*/
 app.delete('/cities/:name', function(request, response){
-    
-  /*Add an if block that checks whether the cityName provided
-  from app.param() has a valid entry before attempting to delete
-  it from the cities object.*/ 
-  if(cities[request.cityName]){
-      
-  /*Use the built-in JavaScript operator delete (see MDN docs)
-  to remove the property for the city passed as an argument. 
-  Don't forget to use the attribute set in app.param() to look 
-  the city up.*/          
-  delete cities[request.cityName];
   
-  /*Use sendStatus() to respond back with a status code of 200.*/
+  /*Add an if block that checks whether the cityName provided
+  from app.param() has a valid entry before attempting to 
+  delete it from the cities object. If a valid city is not 
+  found, then respond with a 404 HTTP status code using the
+  sendStatus() function.*/
+  if(cities[request.cityName]){
+    
+    /*Use the built-in JavaScript operator delete (see MDN docs) 
+  to remove the property for the city passed as an argument. 
+  Don't forget to use the attribute set in app.param() to 
+  look the city up.*/
+  delete cities[request.cityName];
+    
+   // Use sendStatus() to respond back with a status code of 200.
   response.sendStatus(200);
-    }else {
-        
-      /*If a valid city is not found, then respond with a 404 
-      HTTP status code using the sendStatus() function.*/
+    } else {
       response.sendStatus(404);
     }
+  
 });
+
 
 app.listen(3000);
 
